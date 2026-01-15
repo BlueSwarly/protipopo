e34a9b2506937ae30c37ad2678b2c326f4a276bd
BlueSwarly <00067425@uca.edu.sv>
2026-01-15 16:22:16 -0600

    Funciones

---
+++ Paulaner.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+void Suma(){
+    int A;
+    int B;
+    int total = 0;
+
+     cout << "Ingrese los numeros: "<< endl;
+     cin >> A;
+     cin >> B;
+
+    total= A+B;
+     cout << "El resultado es: " << total <<  endl;
+}
+
+void Resta(){
+    int A;
+    int B;
+    int total = 0;
+
+     cout << "Ingrese los numeros: "<< endl;
+     cin >> A;
+     cin >> B;
+
+    total= A-B;
+     cout << "El resultado es: " << total <<  endl;
+}
+
+void Multiplicacion(){
+    int A;
+    int B;
+    int total = 0;
+
+     cout << "Ingrese los numeros: "<< endl;
+     cin >> A;
+     cin >> B;
+
+    total= A*B;
+     cout << "El resultado es: " << total <<  endl;
+}
+
+void Division(){
+    int A;
+    int B;
+    int total = 0;
+
+     cout << "Ingrese los numeros: "<< endl;
+     cin >> A;
+     cin >> B;
+
+    if (B==0){
+         cout << "Error el denominador es cero"<< endl;
+    } else {
+    total= A/B;
+     cout << "El resultado es: " << total <<  endl;
+}
+}
